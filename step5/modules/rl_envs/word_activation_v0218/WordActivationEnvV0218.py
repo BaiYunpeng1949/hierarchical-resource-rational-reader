33605c462045faaa0acb1054ad09edcdb0197e5c
Bai Yunpeng Aalto EE <915840469@qq.com>
2025-02-19 05:45:56 +0200

    Train >> PPO_63 >> Deterministic word activation.

--- step5/modules/rl_envs/word_activation_v0218/WordActivationEnvV0218.py
+++ step5/modules/rl_envs/word_activation_v0218/WordActivationEnvV0218.py
@@ -272,8 +272,8 @@ class WordActivationRLEnv(Env):
     
     def _terminate_step(self):
         self._word_to_activate = self.transition_function.activate_a_word(
-            normalized_belief_distribution_dict=self._normalized_belief_distribution_dict_for_parallel_activation_with_k_words, deterministic=False
-        ) # TODO change to True to try again later
+            normalized_belief_distribution_dict=self._normalized_belief_distribution_dict_for_parallel_activation_with_k_words, deterministic=True
+        )
             
         reward = self.reward_function.get_terminate_reward(
             word_to_recognize=self._word,
