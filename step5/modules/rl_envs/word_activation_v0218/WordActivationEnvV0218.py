f2b4c886e624da402796b4f1ef3c235be1ebdb40
Bai Yunpeng Aalto EE <915840469@qq.com>
2025-02-20 07:22:57 +0200

    Train >> PPO_66, Set the word activation as false (stochastic).

--- step5/modules/rl_envs/word_activation_v0218/WordActivationEnvV0218.py
+++ step5/modules/rl_envs/word_activation_v0218/WordActivationEnvV0218.py
@@ -282,7 +282,7 @@ class WordActivationRLEnv(Env):
     
     def _terminate_step(self):
         self._word_to_activate = self.transition_function.activate_a_word(
-            normalized_belief_distribution_dict=self._normalized_belief_distribution_dict_parallel_activation_with_k_words, deterministic=True
+            normalized_belief_distribution_dict=self._normalized_belief_distribution_dict_parallel_activation_with_k_words, deterministic=False
         )
             
         reward = self.reward_function.get_terminate_reward(
