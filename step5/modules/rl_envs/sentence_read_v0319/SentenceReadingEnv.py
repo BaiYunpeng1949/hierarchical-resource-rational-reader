048a3330dc0b494350a108d55250a419143a5ed6
Bai Yunpeng Aalto EE <915840469@qq.com>
2025-03-29 18:40:09 +0200

    Code and Train >> PPO_84. Use the original dataset, try a weaker regression bonus mechanism. Objective: whether the regression rate could drop.

--- step5/modules/rl_envs/sentence_read_v0319/SentenceReadingEnv.py
+++ step5/modules/rl_envs/sentence_read_v0319/SentenceReadingEnv.py
@@ -137,9 +137,9 @@ class SentenceReadingEnv(Env):
                 self._regressed_words_indexes.append(self._current_word_index)
                 self._reading_sequence.append(self._current_word_index)
                 # NOTE: plan 1 -- Reset belief to 1 for regressed word -- results: too many regressions
-                self._word_beliefs[self._current_word_index] = 1.0
+                # self._word_beliefs[self._current_word_index] = 1.0
                 # NOTE: plan 2 -- a small increment for regressed word -- results: no skipping and regressions at all
-                # self._word_beliefs[self._current_word_index] = np.clip(self._word_beliefs[self._current_word_index] * 1.5, 0, 1)
+                self._word_beliefs[self._current_word_index] = np.clip(0.5*(self._word_beliefs[self._current_word_index]+1.0), 0, 1)
                 
                 self._previous_word_index = self._current_word_index - 1
             reward = self.reward_function.compute_regress_reward()
