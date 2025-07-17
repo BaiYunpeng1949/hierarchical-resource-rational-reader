f548d5da204a3f6b0d69a9e3c111cda854504af3
Bai Yunpeng Aalto EE <915840469@qq.com>
2025-07-17 21:29:29 +0300

    Test and Simulate >> PPO_25. The agent is adaptive on 30s condition, but no skippings on other conditions. Means: The time pressure is too big; but also saying that the degradation to skipped words are not realistic, human's sentence comprehension should be more damaged if they skipped too much. TODO: come up with a better mechanism of how to describe skipping's damage to understanding; and how to use a proper parameter to describe the balance between understanding and comprehension. Oh! So there are two balances I need to consider -- reading speed  (number of skips) vs comprehension and comprehension vs. time penalties.

--- step5/simulators/simulator_v20250604/sub_models/sentence_read_v0604/SentenceReadingEnv.py
+++ step5/simulators/simulator_v20250604/sub_models/sentence_read_v0604/SentenceReadingEnv.py
@@ -23,8 +23,8 @@ SIXTY_SECONDS_EXPECTED_READING_SPEED = Constants.READING_SPEED
 NINETY_SECONDS_EXPECTED_READING_SPEED = Constants.READING_SPEED * 1.5
 
 # Dataset
-# DATASET = "Ours"      # NOTE: I recommend using this dataset for testing
-DATASET = "ZuCo1.0"       # NOTE: I recommend using this dataset for training 
+DATASET = "Ours"      # NOTE: I recommend using this dataset for testing
+# DATASET = "ZuCo1.0"       # NOTE: I recommend using this dataset for training 
 
 
 class SentenceReadingUnderTimePressureEnv(Env):
@@ -231,7 +231,7 @@ class SentenceReadingUnderTimePressureEnv(Env):
                 print(f"The sampled comprehension vs time pressure is {self._w_comprehension_vs_time_pressure} now -----------------------------------------")
         elif self._mode == "simulate":
             # self._w_skip_degradation_factor = 1.00
-            self._w_comprehension_vs_time_pressure = 0.1
+            self._w_comprehension_vs_time_pressure = 0.5
             print(f"NOTE: set the comprehension vs time pressure to a fixed value when running the simulator! Now the comprehension vs time pressure is {self._w_comprehension_vs_time_pressure}")
 
         # Initialize the log variables
